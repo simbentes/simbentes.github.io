fa461f0ac32c43f8eb38edd5ed927c094c2c8d49
simao-d-bentes <simao-d-bentes@alticelabs.com>
2024-08-13 14:56:52 +0100

    force light mode

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const navItems = {
-  '/': {
-    name: 'home',
+  "/": {
+    name: "home",
   },
-  '/blog': {
-    name: 'blog',
+  "/blog": {
+    name: "blog",
   },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
+  "https://vercel.com/templates/next.js/portfolio-starter-kit": {
+    name: "deploy",
   },
-}
+};
 
 export function Navbar() {
   return (
@@ -26,15 +26,15 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all hover:text-neutral-800 flex align-middle relative py-1 px-2 m-1"
                 >
                   {name}
                 </Link>
-              )
+              );
             })}
           </div>
         </nav>
       </div>
     </aside>
-  )
+  );
 }
